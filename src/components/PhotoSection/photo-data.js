83fc39248a04095d09f3b0d7d09f50c891eb1097
datdh <dat.hr.neu@gmail.com>
2024-12-20 01:07:46 +0700

    feat: update image

--- src/components/PhotoSection/photo-data.js
+++ src/components/PhotoSection/photo-data.js
@@ -3,6 +3,19 @@ import { default as Gallery11, default as GalleryT11 } from 'src/assets/images/g
 import { default as Gallery12, default as GalleryT12 } from 'src/assets/images/gallery-12.jpg';
 import { default as Gallery13, default as GalleryT13 } from 'src/assets/images/gallery-13.jpg';
 import { default as Gallery14, default as GalleryT14 } from 'src/assets/images/gallery-14.jpg';
+import { default as Gallery15, default as GalleryT15 } from 'src/assets/images/gallery-15.jpg';
+import { default as Gallery16, default as GalleryT16 } from 'src/assets/images/gallery-16.jpg';
+import { default as Gallery17, default as GalleryT17 } from 'src/assets/images/gallery-17.jpg';
+import { default as Gallery18, default as GalleryT18 } from 'src/assets/images/gallery-18.jpg';
+import { default as Gallery19, default as GalleryT19 } from 'src/assets/images/gallery-19.jpg';
+import { default as Gallery20, default as GalleryT20 } from 'src/assets/images/gallery-20.jpg';
+import { default as Gallery21, default as GalleryT21 } from 'src/assets/images/gallery-21.jpg';
+import { default as Gallery22, default as GalleryT22 } from 'src/assets/images/gallery-22.jpg';
+import { default as Gallery23, default as GalleryT23 } from 'src/assets/images/gallery-23.jpg';
+import { default as Gallery24, default as GalleryT24 } from 'src/assets/images/gallery-24.jpg';
+import { default as Gallery25, default as GalleryT25 } from 'src/assets/images/gallery-25.jpg';
+import { default as Gallery26, default as GalleryT26 } from 'src/assets/images/gallery-26.jpg';
+import { default as Gallery27, default as GalleryT27 } from 'src/assets/images/gallery-27.jpg';
 import { default as Gallery4, default as GalleryT4 } from 'src/assets/images/gallery-4.jpg';
 import { default as Gallery5, default as GalleryT5 } from 'src/assets/images/gallery-5.jpg';
 import { default as Gallery6, default as GalleryT6 } from 'src/assets/images/gallery-6.jpg';
@@ -40,10 +53,6 @@ export const photos = [
     thumbnail: GalleryT5,
   },
   {
-    original: Gallery6,
-    thumbnail: GalleryT6,
-  },
-  {
     original: Gallery9,
     thumbnail: GalleryT9,
   },
@@ -52,7 +61,55 @@ export const photos = [
     thumbnail: GalleryT12,
   },
   {
-    original: Gallery13,
-    thumbnail: GalleryT13,
+    original: Gallery15,
+    thumbnail: GalleryT15,
+  },
+  {
+    original: Gallery16,
+    thumbnail: GalleryT16,
+  },
+  {
+    original: Gallery17,
+    thumbnail: GalleryT17,
+  },
+  {
+    original: Gallery18,
+    thumbnail: GalleryT18,
+  },
+  {
+    original: Gallery19,
+    thumbnail: GalleryT19,
+  },
+  {
+    original: Gallery20,
+    thumbnail: GalleryT20,
+  },
+  {
+    original: Gallery21,
+    thumbnail: GalleryT21,
+  },
+  {
+    original: Gallery22,
+    thumbnail: GalleryT22,
+  },
+  {
+    original: Gallery23,
+    thumbnail: GalleryT23,
+  },
+  {
+    original: Gallery24,
+    thumbnail: GalleryT24,
+  },
+  {
+    original: Gallery25,
+    thumbnail: GalleryT25,
+  },
+  {
+    original: Gallery26,
+    thumbnail: GalleryT26,
+  },
+  {
+    original: Gallery27,
+    thumbnail: GalleryT27,
   },
 ];
